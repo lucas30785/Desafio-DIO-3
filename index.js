9e204df2390209d6fb392151a3e25d9ad54ad657
lucas30785 <lucas30785@gmail.com>
2023-11-14 09:52:14 -0300

    projeto 3

---
+++ index.js
@@ -0,0 +1,42 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+      this.nome = nome;
+      this.idade = idade;
+      this.tipo = tipo;
+    }
+  
+    atacar() {
+      let ataque = "";
+  
+      switch (this.tipo) {
+        case "mago":
+          ataque = "usou magia";
+          break;
+        case "guerreiro":
+          ataque = "usou espada";
+          break;
+        case "monge":
+          ataque = "usou artes marciais";
+          break;
+        case "ninja":
+          ataque = "usou shuriken";
+          break;
+        default:
+          ataque = "usou um ataque genérico";
+      }
+  
+      console.log(`o ${this.tipo} atacou usando ${ataque}`);
+    }
+  }
+  
+  // Exemplo de uso
+  const mago = new Heroi("Gandalf", 100, "mago");
+  const guerreiro = new Heroi("Aragorn", 35, "guerreiro");
+  const monge = new Heroi("Bruce Lee", 40, "monge");
+  const ninja = new Heroi("Hattori Hanzo", 30, "ninja");
+  
+  mago.atacar();
+  guerreiro.atacar();
+  monge.atacar();
+  ninja.atacar();
+  
